fb203373f4cae7272ffd0484350fca8c4cd8c2b4
Antonio <guezo983@gmail.com>
2016-09-19 13:21:12 +0200

    adds Participation and Rating custom cell

---
+++ Mooviest/Mooviest/ParticipationCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ParticipationCollectionViewCell.swift
+//  Mooviest
+//
+//  Created by Antonio RG on 12/9/16.
+//  Copyright © 2016 Mooviest. All rights reserved.
+//
+
+import UIKit
+
+class ParticipationCollectionViewCell: UICollectionViewCell {
+    var faceImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupComponents()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupComponents() {
+        self.backgroundColor = UIColor.blue
+        faceImageView.contentMode = .scaleToFill
+        
+        addSubview(faceImageView)
+    }
+    
+    func setupConstraints() {
+        faceImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addConstraint(faceImageView.topAnchor.constraint(equalTo: topAnchor))
+        addConstraint(faceImageView.leftAnchor.constraint(equalTo: leftAnchor))
+        addConstraint(faceImageView.widthAnchor.constraint(equalTo: widthAnchor))
+        addConstraint(faceImageView.heightAnchor.constraint(equalTo: heightAnchor))
+        
+    }
+}
