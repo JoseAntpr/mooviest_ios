7095f42f30f44b5bd3bae6523675c68f3b3dc931
Antonio <guezo983@gmail.com>
2016-10-10 13:33:00 +0200

    change profile controller for animations are responsive, refactor tab bar animations, and bugs fixeds

--- Mooviest/Mooviest/AnimatedTabBarController.swift
+++ Mooviest/Mooviest/AnimatedTabBarController.swift
@@ -16,102 +16,83 @@ class AnimatedTabBarController: RAMAnimatedTabBarController {
         super.viewDidLoad()
     }
     
-    func createNavigationController(rootViewController vc: UIViewController)-> UINavigationController {
+    func createNavigationController(rootViewController vc: UIViewController,
+                                    title t: String)-> UINavigationController {
         let nVController = UINavigationController(rootViewController: vc)
         
-        nVController.navigationBar.topItem?.titleView = UIImageView(image: UIImage(named: "title")!.withRenderingMode(.alwaysOriginal))
+        nVController.navigationBar.topItem?.title = t
         nVController.navigationBar.barTintColor = UIColor(netHex: mooviest_red)
-        nVController.navigationItem.backBarButtonItem = nil
+//        nVController.navigationItem.backBarButtonItem = nil
         nVController.navigationBar.barStyle = UIBarStyle.black
         nVController.navigationBar.isTranslucent = false
         
         return nVController
     }
     
+    func createAnimatedTabBarItem(title t: String, animation a: RAMItemAnimation,
+                                  namedIcon ni: String, color c: UIColor,
+                                  colorSelected cs: UIColor)-> RAMAnimatedTabBarItem {
+        let icon =  UIImage(named:ni)
+        let tbItem = RAMAnimatedTabBarItem(
+            title: t,
+            image: icon,
+            selectedImage: icon
+        )
+        tbItem.iconColor = c
+        tbItem.textColor = c
+        tbItem.animation = a
+        tbItem.animation.iconSelectedColor = cs
+        tbItem.animation.textSelectedColor = cs
+        
+        return tbItem
+    }
+    
     func makeTabBar() {
         //tab1 Swipe
+        let tbItem1 = createAnimatedTabBarItem(title: "Swipe", animation: RAMBounceAnimation(),
+                                               namedIcon: "swipe", color: UIColor.white.withAlphaComponent(0.5),
+                                               colorSelected: .white)
         let tab1 = SwipeTabViewController(nibName: nil, bundle: nil)
-        let nVController1 = createNavigationController(rootViewController: tab1)
-        let icon1 =  UIImage(named: "swipe")
-        let tbItem1 = RAMAnimatedTabBarItem(
-            title: "Swipe",
-            image: icon1,
-            selectedImage: icon1
-        )
-        
-        tbItem1.animation = RAMBounceAnimation()
-        tbItem1.animation.iconSelectedColor = UIColor.white
-        tbItem1.animation.textSelectedColor = UIColor.white
-        
-        tbItem1.iconColor = UIColor.white.withAlphaComponent(0.5)
-        tbItem1.textColor = UIColor.white.withAlphaComponent(0.5)
+        let nVController1 = createNavigationController(rootViewController: tab1, title: tbItem1.title!)
         nVController1.tabBarItem = tbItem1
         
         //tab2 Advice
-        
+        let tbItem2 = createAnimatedTabBarItem(title: "Advice", animation: RAMBounceAnimation(),
+                                               namedIcon: "thumb-up", color: UIColor.white.withAlphaComponent(0.5),
+                                               colorSelected: .white)
         let tab2 = ViewController(nibName: nil, bundle: nil)
-        let nVController2 = createNavigationController(rootViewController: tab2)
-        let icon2 =  UIImage(named: "thumb-up")
-        let tbItem2 = RAMAnimatedTabBarItem(
-            title: "Advice",
-            image: icon2,
-            selectedImage: icon2
-        )
-        
-        tbItem2.animation = RAMBounceAnimation()
-        tbItem2.animation.iconSelectedColor = UIColor.white
-        tbItem2.animation.textSelectedColor = UIColor.white
-        
-        tbItem2.iconColor = UIColor.white.withAlphaComponent(0.5)
-        tbItem2.textColor = UIColor.white.withAlphaComponent(0.5)
+        let nVController2 = createNavigationController(rootViewController: tab2, title: tbItem2.title!)
         nVController2.tabBarItem = tbItem2
         
         //tab3 Lists
+        let tbItem3 = createAnimatedTabBarItem(title: "Lists", animation: RAMBounceAnimation(),
+                                               namedIcon: "list", color: UIColor.white.withAlphaComponent(0.5),
+                                               colorSelected: .white)
         let tab3 = ViewController(nibName: nil, bundle: nil)
-        let nVController3 = createNavigationController(rootViewController: tab3)
-        let icon3 =  UIImage(named: "list")
-        let tbItem3 = RAMAnimatedTabBarItem(
-            title: "Lists",
-            image: icon3,
-            selectedImage: icon3
-        )
-        
-        tbItem3.animation = RAMBounceAnimation()
-        tbItem3.animation.iconSelectedColor = UIColor.white
-        tbItem3.animation.textSelectedColor = UIColor.white
-        
-        tbItem3.iconColor = UIColor.white.withAlphaComponent(0.5)
-        tbItem3.textColor = UIColor.white.withAlphaComponent(0.5)
+        let nVController3 = createNavigationController(rootViewController: tab3, title: tbItem3.title!)
         nVController3.tabBarItem = tbItem3
         
         //tab4 Profile
+        let tbItem4 = createAnimatedTabBarItem(title: "Profile", animation: RAMBounceAnimation(),
+                                               namedIcon: "profile", color: UIColor.white.withAlphaComponent(0.5),
+                                               colorSelected: .white)
         let tab4 = ProfileViewController(nibName: nil, bundle: nil)
-        let nVController4 = createNavigationController(rootViewController: tab4)
-        nVController4.isNavigationBarHidden = true
-        let icon4 =  UIImage(named: "profile")
-        let tbItem4 = RAMAnimatedTabBarItem(
-            title: "Profile",
-            image: icon4,
-            selectedImage: icon4
-        )
+        let nVController4 = UINavigationController(rootViewController: tab4)
         
-        tbItem4.animation = RAMBounceAnimation()
-        tbItem4.animation.iconSelectedColor = UIColor.white
-        tbItem4.animation.textSelectedColor = UIColor.white
-        
-        tbItem4.iconColor = UIColor.white.withAlphaComponent(0.5)
-        tbItem4.textColor = UIColor.white.withAlphaComponent(0.5)
+        nVController4.navigationBar.topItem?.title = tbItem4.title!
+        nVController4.navigationBar.barStyle = UIBarStyle.black
+        nVController4.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        nVController4.navigationBar.shadowImage = UIImage()
+        nVController4.navigationBar.isTranslucent = true
         nVController4.tabBarItem = tbItem4
         
         self.viewControllers = [nVController1, nVController2, nVController3, nVController4]
-        self.tabBar.isTranslucent = false
         self.tabBar.barTintColor = UIColor(netHex: mooviest_red)
     }
 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }   
 }
 
--- Mooviest/Mooviest/ProfileViewController.swift
+++ Mooviest/Mooviest/ProfileViewController.swift
@@ -12,58 +12,46 @@ import Kingfisher
 
 
 class ProfileViewController: UIViewController, UIScrollViewDelegate {
+    
+    //offset for animation
+    var offset_HeaderStop:CGFloat!
     var offset_CoverStopScale:CGFloat!
+    var offset_CardProfileStop:CGFloat!
     var offset_BackdropFadeOff:CGFloat!
-    var offset_HeaderStop:CGFloat! // = 80 // At this offset the Header stops its transformations
-    var segmentViewOffset:CGFloat!
-    let offset_B_LabelHeader:CGFloat = 30 // At this offset the Black label reaches the Header
-    let distance_W_LabelHeader:CGFloat = 5 // The distance between the bottom of the Header and the top of the White Label
     
     var user = DataModel.sharedInstance.user
     var heightView:CGFloat!
     var v: ProfileView!
-    var heightNav:CGFloat!
-    let participationCellIdentifier = "participationCollectionViewCell"
-    let ratingCellIdentifier = "ratingCollectionViewCell"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.isHidden = true
         v = ProfileView()
-        
         v.bodyScrollView.delegate = self
-//              
         setupView()
         view.addSubview(v)
         setupConstraints()
-        
-        heightView = view.frame.size.height
-        switch heightView {
-        case 736:
-            offset_CoverStopScale = CGFloat(138)
-            offset_HeaderStop = offset_CoverStopScale*1.12
-            segmentViewOffset = 261
-            print("IPhone 6 plus")
-        case 667:
-            offset_CoverStopScale = CGFloat(120)
-            offset_HeaderStop = offset_CoverStopScale*1.12
-            segmentViewOffset = 238
-            print("IPhone 6")
-        case 568:
-            offset_CoverStopScale = CGFloat(90)
-            offset_HeaderStop = offset_CoverStopScale*1.2
-            segmentViewOffset = 209
-            print("IPhone 5")
-        default:
-            offset_CoverStopScale = CGFloat(90)
-            offset_HeaderStop = offset_CoverStopScale*1.2
-            segmentViewOffset = 209
-            
-        }
-        offset_BackdropFadeOff = offset_CoverStopScale/1.6
-        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        //here extract predominant color
+        self.navigationController?.navigationBar.isTranslucent = true
+        v.bodyScrollView.contentSize.height = view.frame.size.height*1.15+v.barSegmentedView.center.y-v.barSegmentedView.frame.height*1.8
+        calculateOffset()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        v.bodyScrollView.setContentOffset(CGPoint(x:0,y:0), animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+
     func setupView() {
         v.coverImageView.kf_setImage(with: URL(string:  (user?.avatar)!),placeholder: UIImage(named: "contact"))
         v.coverImageView.layer.cornerRadius = view.frame.size.width*0.2
@@ -71,81 +59,54 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         v.barSegmentedControl.addTarget(self, action: #selector(self.changeSelected(sender:)), for: .valueChanged)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        //        let colors = v.headerBackdropImageView.image!.getColors()
-        //
-        //        //        backgroundView.backgroundColor = colors.backgroundColor
-        //        //        mainLabel.textColor = colors.primaryColor
-        //        //        secondaryLabel.textColor = colors.secondaryColor
-        //        //        detailLabel.textColor = colors.detailColor headerBackdropImageView.image?.getColors()
-        //        //        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
-        //        //        let blurView = UIVisualEffectView(effect: blurEffect)
-        //        //        blurView.frame =  v.headerBackdropImageView.frame
-        //        //        v.headerBackdropImageView.addSubview(blurView)
-        //
-        //        v.headerLabel.textColor = colors.backgroundColor
-        //        v.headerView.backgroundColor = colors.primaryColor
-        v.bodyScrollView.contentSize.height = view.frame.size.height+v.barSegmentedView.center.y-v.barSegmentedView.frame.height*1.8
-        
-        
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        v.bodyScrollView.setContentOffset(CGPoint(x:0,y:0), animated: true)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        self.navigationController?.navigationBar.isHidden = true
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func calculateOffset() {
+        offset_HeaderStop = v.headerView.frame.size.height-(self.navigationController?.navigationBar.frame.size.height)!-v.height
+        offset_CoverStopScale = offset_HeaderStop
+        offset_CardProfileStop = offset_HeaderStop+v.profileCardView.frame.height+10
+        offset_BackdropFadeOff = offset_CoverStopScale/1.6
     }
     
     func setupConstraints() {
-        
         v.translatesAutoresizingMaskIntoConstraints = false
         
         view.addConstraint(v.leftAnchor.constraint(equalTo: view.leftAnchor))
         view.addConstraint(v.rightAnchor.constraint(equalTo: view.rightAnchor))
         view.addConstraint(v.topAnchor.constraint(equalTo: view.topAnchor))
         view.addConstraint(v.bottomAnchor.constraint(equalTo: view.bottomAnchor))
-        
-        
-    }
-    
+    }    
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = scrollView.contentOffset.y
-        print(offset)
         if scrollView == v.bodyScrollView {
-           
             let offset = scrollView.contentOffset.y
             var avatarTransform = CATransform3DIdentity
             var headerTransform = CATransform3DIdentity
-            var segmentTransform = CATransform3DIdentity
-             print(offset)
-            // PULL DOWN -----------------
+            var cardTransform = CATransform3DIdentity
+            var scrollTransform = CATransform3DIdentity
             
             if offset < 0 {
                 let headerScaleFactor:CGFloat = -(offset) / v.headerView.bounds.height
                 let headerSizevariation = ((v.headerView.bounds.height * (1.0 + headerScaleFactor)) - v.headerView.bounds.height)/2.0
-                headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+                headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation*0.5, 0)
+                cardTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
                 headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+                scrollTransform = CATransform3DTranslate(cardTransform, 0, offset, 0)
                 
-            } else {    // SCROLL UP/DOWN ------------
+                if v.coverImageView.layer.zPosition < v.headerView.layer.zPosition{
+                    v.headerView.layer.zPosition = 0
+                }
                 
-                // Header -----------
-                
-                headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
-                
-                //  ------------ Blur
-                
+            } else {
+                //Alpha
                 v.headerBackdropImageView.alpha = max (0, (1-( offset-offset_BackdropFadeOff*3)/distance_W_LabelHeader)/10)
                 
-                // Avatar -----------
-                let avatarScaleFactor = (min(offset_CoverStopScale, offset)) / v.coverImageView.bounds.height  // Slow down the animation
+                //Animations
+                headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
+                cardTransform = CATransform3DTranslate(cardTransform, 0, max(-offset_CardProfileStop, -offset), 0)
+                scrollTransform = cardTransform
+                
+                let avatarScaleFactor = (min(offset_CoverStopScale, offset)) / v.coverImageView.bounds.height
                 let avatarSizeVariation = ((v.coverImageView.bounds.height * (1.0 + avatarScaleFactor)) - v.coverImageView.bounds.height)
-                avatarTransform = CATransform3DTranslate(avatarTransform, 0, 0.5*avatarSizeVariation, 0)
+                avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation*0.5, 0)
                 avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
                 
                 if offset <= offset_CoverStopScale {
@@ -159,22 +120,12 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
                         v.backgroundStatusView.layer.zPosition = 2
                     }
                 }
-                
-                // Segment control
-                
-                // Scroll the segment view until its offset reaches the same offset at which the header stopped shrinking
-                //segmentTransform = CATransform3DTranslate(segmentTransform, 0, max(segmentViewOffset, -offset_HeaderStop*1.60), 0)
-                if segmentViewOffset < offset {
-                    
-                    segmentTransform = CATransform3DTranslate(segmentTransform, 0,offset-segmentViewOffset, 0)
-                    //                v.barSegmentedView.layer.transform = segmentTransform
-                    
-                }
             }
-            // Apply Transformations
-            v.barSegmentedView.layer.transform = segmentTransform
+            v.barSegmentedView.layer.transform = cardTransform
             v.headerView.layer.transform = headerTransform
             v.coverImageView.layer.transform = avatarTransform
+            v.profileCardView.layer.transform  = cardTransform
+            v.bodyScrollView.layer.transform  = scrollTransform
         }
     }
     
@@ -199,9 +150,6 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    override var preferredStatusBarStyle : UIStatusBarStyle {
-        return UIStatusBarStyle.lightContent
-    }
     
     
     
